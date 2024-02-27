e4ad1ceb44c28c1e6e5e2377dc5c8eefdf57be4e
ashoshari <161231552+ashoshari@users.noreply.github.com>
2024-02-28 00:01:03 +0300

    Add files via upload

---
+++ i18n.js
@@ -0,0 +1,338 @@
+export const messages = {
+    en: {
+        mainTitle: "The best hotel care products",
+        whyWe: {
+            title: "Why us",
+            par: "Our company provides everything necessary for hotel supplies, cotton, bedspreads, towels, and what is needed for serviced apartments and chalets."
+        },
+        hotelEq: {
+            title: "Hotel equipment",
+            par: "Hotel supplies meet international standards and are compatible with modern developments and distinguished services"
+        },
+        highQuality: {
+            title: "high quality",
+            par: "All hotel products are of international quality and standard specifications"
+        },
+        suitablePrices: {
+            title: "Suitable prices",
+            par: "Catering to all types of hotels, furnished apartments and chalets at competitive prices and fast service"
+        },
+        delivery: {
+            title: "Delivery throughout the Kingdom",
+            par: "Meet your needs for hotel supplies and equipment as quickly as possible, Regardless of your location"
+        },
+        logoMake: {
+            title: "التصنيع بشعاركم",
+            par: "تصنيع المنتجات الفندقية والمستلزمات خصيصا لكم ووضع شعارك عليها حفاظا على هويتكم"
+        },
+        cottonReq: {
+            title: "طلبيات قطنية",
+            par: "تغيير كافة القطنيات المفارش والمناشف بمقاييسكم ومقاساتكم الخاصة بأسرع وقت وأفضل الاسعار"
+        },
+        aboutus: {
+            about1: "who are we ?? Eagles Hospitality Foundation, Jeddah, specializes in hospitality supplies, hotel supplies, and equipment for hotels, furnished apartments, suites, chalets, and restaurants. What is the importance of the role that we contribute to achieving for clients of hotels, hotel apartments, and chalets, as it is a decisive role in achieving a distinctive experience for the guests. Hotels can equip accommodations with the best equipment and tools to ensure an unforgettable experience for guests. Nusour Al Diyafah Foundation works in the field of hotel supplies and hotel equipment to provide many benefits to customers. These benefits include fast and reliable delivery service, ensuring product quality, providing diverse and customized options to meet customer needs, and understanding and helpful customer service.",
+            about2: "Our vision?? We at Eagles Hospitality Foundation aim to provide a wide range of products and services that ensure comfort and luxury in hotels. These products include skin care and body care supplies, personal care, and more. More bedding, towels, and cotton services that meet your needs. Nosour Al-Dhiyafah Foundation works in the field of hotel supplies, furnished apartments, and chalets with many privileges for customers. These benefits include fast and reliable delivery, ensuring product quality, providing diverse and customized options to meet customers' needs, and understanding and helpful customer service. In addition, special price offers and discounts are sent to distinguished customers on a weekly basis. To give the customer the ability to compare prices, whichever is lower, and to be able to make decisions more accurately and appropriately. We aim to satisfy customers, enhance their satisfaction and personal experience, and work in a more forward-looking reality",
+        },
+        featureP: "",
+        reviews: "Reviews",
+        yourReview: "Your review",
+        header: {
+            home: "Home",
+            shop: "Shop",
+            contact: "Contact us",
+            about: "About us",
+            login: "Login",
+            register: "Register",
+            company: "Eagles Hospitality",
+            companyCompliment:
+                "All necessary hospitality supplies to equip hotels, serviced apartments, chalets and hospitals",
+            catalog: "Catalog",
+        },
+        rateOurProduct: "Rate our product",
+
+        comment: "Comment",
+        shopNow: {
+            title: "SHOP NOW",
+        },
+        securePayment: {
+            title: "Security Payment",
+            explaination: "100% Security Payment",
+        },
+        dayReturn: {
+            title: "10 Day Return",
+            explaination: "10 day money guarantee",
+        },
+        support: {
+            title: "24/7 Support",
+            explaination: "Support every time fast",
+        },
+        products: {
+            title: "Products",
+            deleteProductSure:
+                "Are you sure you want to delete the selected product?",
+            deleteProductsSure:
+                "Are you sure you want to delete the selected products?",
+            update: "Update",
+            bestSelling: "BESTSELLER",
+            size: "Size :",
+            quantity: "Quantity per Packet :",
+            category1: "Skin care",
+            category2: "Personal care",
+            category3: "Body care",
+            category4: "Extra care",
+            category5: "Towels",
+            category6: "Mattresses",
+
+            shopCategory: "Categories",
+        },
+        bestSellingProducts: {
+            title: "Bestseller Products",
+        },
+        satisfiedCustomers: {
+            title: "satisfied customers",
+        },
+        qualityOfService: {
+            title: "Quality of Service",
+        },
+        qualityCertificates: {
+            title: "quality certificates",
+        },
+        availableProducts: {
+            title: "Available Products",
+        },
+        footer: {
+            address: "Address : Saudi Arabia - Jeddah",
+            email: "Email :",
+            PaymentAccepted: "Payment Accepted",
+            contact: "Contact us",
+            account: "Account",
+            shopInfo: "Shop Info",
+            readMore: "Read More",
+            copyRights: "All rights reserved.",
+            callUs: "Call us :",
+        },
+        profile: {
+            title: "Profile",
+            profileInfo: "Profile Information",
+            profileInfoParagraph:
+                "Update your account's profile information and email address.",
+            profileInfoName: "Name",
+            profileInfoEmail: "Email",
+            updatePassword: "Update Password",
+            updatePasswordParagraph:
+                "Ensure your account is using a long, random password to stay secure.",
+            updatePasswordCurrent: "Current Password",
+            updatePasswordNew: "New Password",
+            updatePasswordConfirm: "Confirm Password",
+            deleteAccount: "Delete Account",
+            deleteAccountConfirmTitle:
+                "Are you sure you want to delete your account?",
+            deleteAccountConfirmParagraph1:
+                "Once your account is deleted, all of its resources and data will be permanently deleted.",
+            deleteAccountConfirmParagraph2:
+                "Please enter your password to confirm you would like to permanently delete your account.",
+            password: "password",
+            cancel: "cancel",
+            save: "Save",
+            saved: "Saved",
+            profileImage: "Profile Image",
+            alreadyRegistered: "Already registered ?",
+            rememberMe: "Remember me",
+        },
+        logout: {
+            title: "Logout",
+        },
+        contact: {
+            title: "Address",
+            address: "Saudi Arabia - Jeddah",
+            mailUs: "Mail Us",
+            gmail: "zaidghzawi118{'@'}gmail.com",
+            telephoneTitle: "Telephone",
+            telephone: "+966540349846",
+
+            formName: "Enter your Name",
+            formEmail: "Enter your Email",
+            formMessage: "Your Message",
+            formSubmit: "Send",
+        },
+        createProduct: {
+            category: "Category",
+            nameEnglish: "Name English",
+            nameArabic: "Name Arabic",
+            descEnglish: "Description English",
+            descArabic: "Description Arabic",
+            size: "Size",
+            QuantityPerPacket: "Quantity per Packet",
+            bestSelling: "Best Selling",
+            bestSellingOptions: {
+                yes: "Yes",
+                no: "No",
+            },
+            create: "Create",
+            cancel: "Cancel",
+        },
+        productRate: "Product rate :",
+    },
+    ar: {
+        mainTitle: "أفضل منتجات العناية الفندقية",
+
+        whyWe: {
+            title: "لماذا نحن",
+            par: "مؤسستنا توفر كل ما يلزم التجهيزات الفندقية والقطنيات والمفارش والمناشف وما يلزم الشقق المخدومة والشاليهات"
+        }, hotelEq: {
+            title: "تجهيزات فندقية",
+            par: "مستلزمات الفنادق بالمعايير العالمية وتتلائم مع التطورات العصرية والخدمات المتميزة"
+        },
+        highQuality: {
+            title: "جودة عالية",
+            par: "كل المنتجات الفندقية بجودة عالمية ومواصفات قياسية"
+        },
+        suitablePrices: {
+            title: "أسعار مناسبة",
+            par: "تلبية كافة تجهيز الفنادق والشقق المفروشة والشاليهات بأسعار منافسة وخدمة سريعة"
+        },
+        delivery: {
+            title: "التوصيل كافة أنحاء المملكة",
+            par: "تلبية احتياجاتكم من مستلزمات وتجهيزات فندقية بأسرع وقت ممكن وأين كان موقعكم"
+        },
+        aboutus: {
+            about1: "من نحن ؟؟  مؤسسة نسور الضيافة جدة مختصون في مستلزمات الضيافة والمستلزمات الفندقية وتجهيزات الفنادق والشقق المفروشة والاجنحة والشاليهات والمطاعم وما أهمية الدور الذي نسهم في تحقيقه للعملاء من الفنادق والشقق الفندقية والشاليهات كونه دورًا حاسمًا في تحقيق تجربة مميزة للنزلاء. يمكن للفنادق تجهيز أماكن الإقامة بأفضل التجهيزات والأدوات لضمان تجربة لا تُنسى للنزلاء تعمل مؤسسة نسور الضيافة في مجال الإمدادات الفندقية وتجهيزات الفنادق على العديد من الامتيازات للعملاء. تشمل هذه الامتيازات خدمة توصيل سريعة وموثوقة، ضمان جودة المنتجات، توفير خيارات متنوعة ومخصصة لتلبية احتياجات العملاء، وخدمة عملاء متفهمة ومتعاونة",
+            about2: "رؤيتنا ؟؟ نهدف نحن في مؤسسة نسور الضيافة إلى توفير مجموعة واسعة من المنتجات والخدمات التي تضمن الراحة والفخامة في الفنادق. تشمل هذه المنتجات ،مستلزمات العناية بالبشرة والعناية بالجسم , والعناية الشخصية والمزيد المزيد من خدمات المفارش والمناشف والقطنيات التي تلبي احتياجاتكم تعمل مؤسسة نسور الضيافة في مجال التجهيزات الفندقية وتجهيزات والشقق المفروشة والشاليهات على العديد من الامتيازات للعملاء. تشمل هذه الامتيازات خدمة توصيل سريعة وموثوقة، ضمان جودة المنتجات، توفير خيارات متنوعة ومخصصة لتلبية احتياجات العملاء، وخدمة عملاء متفهمة ومتعاونة. بالإضافة إلى ذلك، يتم ارسال عروض الأسعار والخصومات الخاصة للعملاء المميزين اسبوعيا بشكل مستمر . لاعطاء العميل امكانية المفاضلة في الأسعار أيهما أقل ويتمكن من اتخاذ القرارات بشكل أدق ومناسب نهدف إلى إرضاء العملاء وتعزيز رضاهم وتجربتهم الشخصية والعمل في واقع أكثر تطلعا"
+        },
+        yourReview: "قيّم هذا المنتج",
+        reviews: "التقييم",
+        productRate: "تقييم المنتج",
+        rateOurProduct: "قيّم هذا المنتج",
+
+        shopNow: {
+            title: "تسوق الآن",
+        },
+        comment: "تعليق",
+
+        securePayment: {
+            title: "دفع آمن",
+            explaination: "دفع آمن %100",
+        },
+        dayReturn: {
+            title: "ضمان 10 أيام",
+            explaination: "ضمان استرجاع المال لمدة 10 أيام",
+        },
+        support: {
+            title: "دعم 24/7",
+            explaination: "دعم لا محدود واستجابة سريعة",
+        },
+        products: {
+            title: "المنتجات",
+            deleteProductSure: "هل أنت متأكد من أنك تريد حذف هذا المنتج؟",
+            deleteProductsSure: "هل أنت متأكد من أنك تريد حذف هذا المنتجات",
+            update: "تحديث",
+            bestSelling: "من الأفضل مبيعاً",
+            size: "الحجم :",
+            quantity: "الكمية بالكرتونة :",
+            category1: "العناية بالبشرة",
+            category2: "العناية الشخصية",
+            category3: "العناية بالجسم",
+            category4: "العناية الإضافية",
+            category5: "مناشف",
+            category6: "مفارش",
+            shopCategory: "الأقسام",
+        },
+        bestSellingProducts: {
+            title: "أفضل المنتجات مبيعا",
+        },
+        satisfiedCustomers: {
+            title: "زبائن يشعرون بالرضى",
+        },
+        qualityOfService: {
+            title: "ضمان الجودة",
+        },
+        qualityCertificates: {
+            title: "شهادات الجودة",
+        },
+        availableProducts: {
+            title: "المنتجات المتوفرة",
+        },
+        header: {
+            home: "الرئيسية",
+            shop: "المتجر",
+            contact: "تواصل معنا",
+            about: "من نحن",
+            login: "تسجيل الدخول",
+            register: "أنشئ حساب",
+            company: "مؤسسة نسور الضيافة",
+            companyCompliment:
+                "كل ما يلزم مستلزمات الضيافة لتجهيز الفنادق والشقق المخدومة والشاليهات والمستشفيات",
+            catalog: "كتالوج",
+        },
+        footer: {
+            address: "العنوان : المملكة العربية السعودية - جدة",
+            email: " البريد الإلكتروني :",
+            PaymentAccepted: "طرق الدفع المتاحة :",
+            contact: "تواصل معنا",
+            account: "معلومات الحساب",
+            shopInfo: "التسوق الإلكتروني",
+            readMore: "إقرأ المزيد",
+            copyRights: "جميع الحقوق محفوظة",
+            callUs: "إتصل بنا :",
+        },
+        profile: {
+            title: "حسابك الشخصي",
+            profileInfo: "معلومات الحساب",
+            profileInfoParagraph:
+                "تغيير معلومات الحساب الشخصي والبريد الإلكتروني",
+            profileInfoName: "الإسم",
+            profileInfoEmail: "البريد الإلكتروني",
+            updatePassword: "تغيير كلمة المرور",
+            updatePasswordParagraph:
+                "يرجى إدخال كلمة مرور قوية ومكونة من أحرف وأرقام ",
+            updatePasswordCurrent: "كلمة المرور الحالية",
+            updatePasswordNew: "كلمة المرور الجديدة",
+            updatePasswordConfirm: "تأكيد كلمة المرور",
+            deleteAccount: "حذف الحساب",
+            deleteAccountConfirmTitle:
+                "هل أنت متأكد أنك تريد حذف حسابك الشخصي ؟",
+            deleteAccountConfirmParagraph1:
+                ".بمجرد حذف حسابك الشخصي، سوف يتم حذف جميع المصادر والمعلومات المتعلقة بحسابك",
+            deleteAccountConfirmParagraph2: "يرجى إدخال كلمة المرور للتأكيد ",
+            password: "كلمة المرور",
+            cancel: "تراجع",
+            save: "حفظ",
+            saved: "تم الحفظ",
+            profileImage: "صورة الملف الشخصي",
+            alreadyRegistered: "لديك حساب ؟",
+            rememberMe: "تذكرني",
+        },
+        logout: {
+            title: "تسجيل خروج",
+        },
+
+        contact: {
+            title: "العنوان",
+            address: "المملكة العربية السعودية - جدة",
+            mailUs: "تواصل معنا على البريد الإلكتروني",
+            gmail: "zaidghzawi118{'@'}gmail.com",
+            telephoneTitle: "الهاتف",
+            telephone: "+966540349846",
+            formName: "الإسم",
+            formEmail: "البريد الإلكتروني",
+            formMessage: "رسالتك",
+            formSubmit: "إرسال",
+        },
+        createProduct: {
+            category: "الصنف",
+            nameEnglish: "إسم إنجليزي",
+            nameArabic: "إسم عربي",
+            descEnglish: "وصف إنجليزي",
+            descArabic: "وصف عربي",
+            size: "الحجم",
+            QuantityPerPacket: "الكمية بالكرتونة",
+            bestSelling: "أفضل المنتجات مبيعا",
+            bestSellingOptions: {
+                yes: "نعم",
+                no: "لا",
+            },
+            create: "إنشاء",
+            cancel: "تراجع",
+        },
+    },
+};
